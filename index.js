22c3b739bb9930762c6924b9253e4f3d9787c2aa
yonatand1230 <yonatand1230@gmail.com>
2022-06-14 09:42:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const qrcode = require('qrcode-terminal');
+const { Client, LocalAuth } = require('whatsapp-web.js');
+
+const client = new Client({
+    authStrategy: new LocalAuth()
+});
+client.on("qr", qr => {
+    qrcode.generate(qr, {small: true});
+});
+
+client.on("ready", () => {
+    console.log("Ready!");
+});
+
+client.on("message", message => {
+	if(message.body === "שלום!") {
+		client.sendMessage(message.from, "עגכטעכטגעכטעגטכ אני רובוט בופ ביפ באפ");
+	}
+});
+
+client.on("message", message => {
+	if(message.body === "דגה") {
+		client.sendMessage(message.from, "מה?");
+	}
+});
+
+client.on("message", async (msg) => {
+    const mentions = await msg.getMentions();
+    
+    for(let contact of mentions) {
+        client.sendMessage(msg.from, "מישהו קרא לי?")
+    }
+});
+
+
+
+
+client.initialize();
